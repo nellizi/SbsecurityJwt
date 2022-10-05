03f0b950a32bb8e84031ac96d9aa132f61da2e4e
nellizi <masterliz9095@gmail.com>
2022-10-06 01:30:04 +0900

    ResponseEntity 로 헤더 추가

--- src/main/java/com/ll/exam/app__2022_10_05/app/member/controller/MemberController.java
+++ src/main/java/com/ll/exam/app__2022_10_05/app/member/controller/MemberController.java
@@ -2,6 +2,9 @@ package com.ll.exam.app__2022_10_05.app.member.controller;
 
 
 import lombok.Data;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,9 +16,13 @@ import javax.servlet.http.HttpServletResponse;
 @RequestMapping("/member")
 public class MemberController {
     @PostMapping("/login")
-    public String login(@RequestBody LoginDto loginDto, HttpServletResponse response) {
-        response.addHeader("Authentication", "JWT토큰");
-        return "username : %s, password : %s".formatted(loginDto.getUsername(), loginDto.getPassword());
+    public ResponseEntity<String> login(@RequestBody LoginDto loginDto) {
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("Authentication", "JWT키");
+
+        String body = "username : %s, password : %s".formatted(loginDto.getUsername(), loginDto.getPassword());
+
+        return new ResponseEntity<>(body, headers, HttpStatus.OK);
     }
 
     @Data
